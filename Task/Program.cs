0977a4bb3b8fa62854757775bd088db3548d622a
Valentine Borisevich <valentineborisevich@gmail.com>
2023-02-18 20:48:52 +0300

    methods added

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillStringArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Enter string");
+        array[i] = Console.ReadLine();
+    }
+}
+
+string[] PrintStringArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        // Console.Write("["+array[i] + "] ");
+        Console.Write($"'{array[i]}' ");
+    }
+    return array;
+}
